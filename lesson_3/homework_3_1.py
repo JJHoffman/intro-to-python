a89c645cf1567447241ef2b707375d2f733f795f
jj <jj@jjhoffmanconsulting.com>
2023-02-14 14:08:57 -0800

    lesson 3.1 progress

--- lesson_3/homework_3_1.py
+++ lesson_3/homework_3_1.py
@@ -1,32 +1,45 @@
 # Enter two numbers. If the first one is greater than the second, save first number in result_1,
 # otherwise save the second number to the result_1 variable.
 
-first_number = None
-second_number = None
-result_1 = None
+# first_number = 4
+# second_number = 2
+# if first_number > second_number:
+#     result_1 = first_number
+# else:
+#     result_1 = second_number
+# print(result_1)
 
 # Enter a random number in number_1 variable. If this number is 20 or
 # higher save “Too high” text to result_2, otherwise save “Thank you”.
-number_1 = None
-result_2 = None
-
+# number_1 = 10
+# if number_1 >= 20:
+#     result_2 = "Too High"
+# else:
+#     result_2 = "Thank you"
+# print(result_2)
 
 # Enter your first name and last name in first_name and last_name variables. If the length of your first name is under
 # five characters, join them together (without a space) and save it to result_3 variable in upper case. If the length
 # of the first name is five or more characters, save their first name in lower case in result_3 variable.
 
-first_name = None
-last_name = None
-result_3 = None
-
+# first_name = "JJ"
+# last_name = "Hoffman"
+# print(first_name + last_name) if len(first_name) < 5 else print(first_name.lower())
+#
+#
 
 # Enter a number between 10 and 20 (inclusive) and save number to number_2 variable
 # If they enter a number within this range, save a message “Thank you” to result_4, otherwise a
 # message “Incorrect answer” to result_4.
 
-number_2 = None
-result_4 = None
-
+# number_2 = int( input('enter a number between 10-20 inclusive: '))
+# if number_2 < 10:
+#     result_4 = "Incorrect answer"
+# elif number_2 > 20:
+#     result_4 = "Incorrect answer"
+# else:
+#     result_4 = "Thank You"
+# print(result_4)
 
 # Enter your age. If you are 18 or over, save the message “You can vote” in result_5,
 # if you are aged 17, save the message “You can learn to drive” in result_5 variable,
@@ -36,9 +49,8 @@ result_4 = None
 age = None
 result_5 = None
 
-
 # Enter a number between 1 and 12, save this value to month variable. Find which month is it.
-# (January, February, March, April, May, June, Jule, August, September, October, November, December)
+# (January, February, March, April, May, June, July, August, September, October, November, December)
 # Write answer in result_month in lower case
 
 month = None
